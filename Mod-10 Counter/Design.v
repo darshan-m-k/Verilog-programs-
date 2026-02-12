8c112b4daf89694233d55fcd7d093ad9620a8488
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-12 16:54:23 +0530

    mod10_counter design.v

--- Mod-10 Counter/Design.v
+++ Mod-10 Counter/Design.v
@@ -2,12 +2,15 @@ module mod10_counter(clk,rst,count);
   input clk,rst;
   output reg[3:0]count;
   reg[3:0] temp;
-  always@(posedge clk)
-    begin
+  
+  always@(posedge clk)begin
+    
       if(!rst)
         temp=0;
+    
       else if(temp<9)
         temp=temp+1;
+    
       count=temp;
     end
 endmodule
