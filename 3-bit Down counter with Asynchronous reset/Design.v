d0f41ee987f523ed624ad4235e08bd3b33ed8781
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-12 16:43:42 +0530

    down counter design.v

--- 3-bit Down counter with Asynchronous reset/Design.v
+++ 3-bit Down counter with Asynchronous reset/Design.v
@@ -1,14 +1,12 @@
 module down_counter_3bit_async (
-    input  wire clk,
-    input  wire rst,          // asynchronous reset (active high)
-    output reg  [2:0] count
-);
+    input  wire clk,rst,          // asynchronous reset (active high)
+    output reg  [2:0] count);
 
     always @(posedge clk or posedge rst) begin
         if (rst)
             count <= 3'b111;   // start from 7
+        
         else
             count <= count - 1'b1;
     end
-
 endmodule
