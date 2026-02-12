120b5d39b31bb71b9821f725e8ca982a5c060b94
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-12 16:45:35 +0530

    down counter testbench.v
    Refactor testbench for down counter with improved monitoring and waveform dumping.

--- 3-bit Down counter with Asynchronous reset/Testbench.v
+++ 3-bit Down counter with Asynchronous reset/Testbench.v
@@ -1,14 +1,9 @@
-module tb_down_counter_3bit_async;
-
+module down_counter_3bit_async_tb;
     reg clk, rst;
     wire [2:0] count;
 
-    down_counter_3bit_async DUT (
-        .clk(clk),
-        .rst(rst),
-        .count(count)
-    );
-
+    down_counter_3bit_async DUT (clk,rst,count);
+       
     // Clock generation
     always #5 clk = ~clk;
 
@@ -16,9 +11,17 @@ module tb_down_counter_3bit_async;
         clk = 0;
         rst = 1;
 
-        #12 rst = 0;   // release reset
+        #12 rst = 0;  
 
-        #200 $finish;
+        #80 $finish;
+    end
+    
+  initial begin
+        $monitor("Time=%0t | Reset=%b | Count=%b", $time,rst,count);
     end
 
+    initial begin
+        $dumpfile("counter.vcd");
+        $dumpvars(0, down_counter_3bit_async_tb);
+    end
 endmodule
