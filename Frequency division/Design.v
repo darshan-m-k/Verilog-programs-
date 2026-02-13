320552d88f47aa1dc1b8050d62174bb5056f4df8
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-13 11:47:38 +0530

    Add files via upload

---
+++ Frequency division/Design.v
@@ -0,0 +1,26 @@
+module FD#(parameter N= 4) (input clk, rst, output clk_out);
+reg int_clk; integer count;
+assign clk_out= int_clk;
+always@(posedge clk or negedge clk) // always@(clk)
+begin
+
+if(rst)
+
+begin
+
+int_clk= 0;
+
+count = 0;
+
+end
+
+else
+
+count= count+1;
+if(count%N==0)
+begin int_clk= ~int_clk;
+count= 0;
+end
+end
+endmodule
+
---
+++ Frequency division/Testbench.v
@@ -0,0 +1,16 @@
+//----------TB----------
+module fd_test;
+reg rst, clk; wire clk_out;
+FD dut (clk,rst, clk_out) ; 
+initial begin
+rst= 1;
+ clk= 0;
+#12 rst= 0;
+#100 $stop;
+end
+always #5 clk= ~clk;
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,clk,rst,clk_out);
+  end
+endmodule
