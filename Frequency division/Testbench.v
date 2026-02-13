382501cdb628840ec601bafb2777a7030d621356
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-13 11:51:34 +0530

    Frequency division testbench.v

--- Frequency division/Testbench.v
+++ Frequency division/Testbench.v
@@ -1,16 +1,23 @@
 //----------TB----------
+
 module fd_test;
-reg rst, clk; wire clk_out;
+ reg rst, clk; 
+ wire clk_out;
+ 
 FD dut (clk,rst, clk_out) ; 
+ 
 initial begin
-rst= 1;
+ rst= 1;
  clk= 0;
-#12 rst= 0;
-#100 $stop;
+ #12 rst= 0;
+ #100 $stop;
 end
+ 
 always #5 clk= ~clk;
+ 
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0,clk,rst,clk_out);
   end
 endmodule
+
