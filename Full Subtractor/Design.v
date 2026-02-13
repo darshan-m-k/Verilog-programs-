0e629b244e06538efebca366c23917252de8d2df
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-13 11:58:24 +0530

    Full Subtractor Design.v

--- Full Subtractor/Design.v
+++ Full Subtractor/Design.v
@@ -1,12 +1,10 @@
-module full_subtractor (
-    input A,
-    input B,
-    input Bin,
-    output D,
-    output Bout
-);
+//Dataflow Modelling
+
+module full_subtractor(
+    input A,B,Bin,
+    output D,Bout);
 
     assign D = A ^ B ^ Bin;
     assign Bout = (~A & B) | (~A & Bin) | (B & Bin);
-
 endmodule
+
