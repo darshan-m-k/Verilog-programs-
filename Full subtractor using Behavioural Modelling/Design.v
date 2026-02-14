40973a28bfedceb761f22e3c9def5fdb4581b38b
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-14 19:31:52 +0530

    Update Design.v

--- Full subtractor using Behavioural Modelling/Design.v
+++ Full subtractor using Behavioural Modelling/Design.v
@@ -1,15 +1,10 @@
 module full_subtractor (
-    input A,
-    input B,
-    input Bin,
-    output reg D,
-    output reg Bout
-);
+    input A,B,Bin,
+    output reg D,Bout);
 
   always @(A,B,Bin) begin
-       
-        D = A - B - Bin;
-        Bout = (~A & B) | (~A & Bin) | (B & Bin);
+      D = A - B - Bin;
+      Bout = (~A & B) | (~A & Bin) | (B & Bin);
     end
-
 endmodule
+
