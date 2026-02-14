3aa2e4f816b9f51edac1b084ec83c30446ca0156
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-14 19:39:55 +0530

    Update Design.v

--- Fibonacci Series/Design.v
+++ Fibonacci Series/Design.v
@@ -1,24 +1,21 @@
 module fibonacci_series;
-
     integer a[0:9]; 
     integer i;
-
+    
     initial begin
-        
         a[0] = 0;
         a[1] = 1;
-
         
         for (i = 2; i < 10; i = i + 1) begin
           a[i] = a[i - 1] + a[i - 2];
         end
       
         $display("Fibonacci Series up to 10th position:");
+        
         for (i = 0; i < 10; i = i + 1) begin
           $display("a[%0d] = %0d", i, a[i]);
         end
 
         $finish;
     end
-
-endmodule
+        endmodule
