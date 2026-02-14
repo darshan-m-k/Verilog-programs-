ea853526df748003b186d9bcf0357f7d14bd490b
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-14 19:35:55 +0530

    Update Testbench.v

--- Full subtractor using Structural Modelling/Testbench.v
+++ Full subtractor using Structural Modelling/Testbench.v
@@ -1,15 +1,8 @@
-module tb_full_subtractor;
-    reg A;
-    reg B;
-    reg Bin;
+module full_subtractor_tb;
+    reg A,B, Bin;
+    wire D,Bout;
 
-    
-    wire D;
-    wire Bout;
-
-   
-  full_subtractor dut (A,B,Bin,D,Bout);
-
+    full_subtractor dut (A,B,Bin,D,Bout);
     
     initial begin
         $display("A B Bin | D Bout");
@@ -23,8 +16,10 @@ module tb_full_subtractor;
     #10;	A = 1'b1; B = 1'b1; Bin = 1'b0; 
     #10;    A = 1'b1; B = 1'b1; Bin = 1'b1;
   end
+    
   initial begin
     $monitor("simtime=%0t,A=%b,B=%b,Bin=%b,s=%b,c=%b", $time, A,B, Bin,D,Bout);
   end
-endmodule
+      endmodule
+
   
