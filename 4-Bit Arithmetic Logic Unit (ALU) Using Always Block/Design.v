e9cb30fd77ceac7638d3609fba3a45c7fcdeba44
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-15 13:56:49 +0530

    Update Design.v

--- 4-Bit Arithmetic Logic Unit (ALU) Using Always Block/Design.v
+++ 4-Bit Arithmetic Logic Unit (ALU) Using Always Block/Design.v
@@ -1,25 +1,32 @@
-// Code your design here
 module ALU (A,B,Sel,ALU_Out);
   input [3:0] A;     
   input [3:0] B;
   input [2:0]Sel;
-  output reg [3:0] ALU_Out;   
-always @(A,B) begin
-  if(Sel==3'b000)
+  output reg [3:0] ALU_Out;
+  
+  always @(A,B) begin
+    if(Sel==3'b000)
     ALU_Out = A + B; 
-  else if(Sel==3'b001)
-    ALU_Out = A - B; 
-  else if(Sel==3'b010)
+    
+    else if(Sel==3'b001)
+    ALU_Out = A - B;
+    
+    else if(Sel==3'b010)
     ALU_Out = A & B; 
-  else if(Sel==3'b011)
+    
+    else if(Sel==3'b011)
     ALU_Out = A | B;
-  else if(Sel==3'b100)
+    
+    else if(Sel==3'b100)
      ALU_Out = A ^ B;
-  else if(Sel==3'b101)
+    
+    else if(Sel==3'b101)
      ALU_Out = ~(A^B);
-  else if(Sel==3'b110)
-      ALU_Out = ~A; 
-  else if(Sel==3'b111)
+    
+    else if(Sel==3'b110)
+      ALU_Out = ~A;
+    
+    else if(Sel==3'b111)
       ALU_Out = A;               
     end
-endmodule
+    endmodule
