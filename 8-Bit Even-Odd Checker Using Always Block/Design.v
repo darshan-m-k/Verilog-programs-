736657de434b08999780c0e07e0f0ed4992deefe
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-15 13:45:16 +0530

    Update Design.v

--- 8-Bit Even-Odd Checker Using Always Block/Design.v
+++ 8-Bit Even-Odd Checker Using Always Block/Design.v
@@ -1,18 +1,15 @@
 module even_odd_checker(
     input [7:0] n,  
-    output reg even,  
-    output reg odd    
-);
+    output reg even,odd);
 
     always @(n) begin
-        
         if (n % 2 == 0) begin
             even = 1;  
-            odd = 0;   
-        end else begin
-            even = 0;  
-            odd = 1;   
+            odd = 0;
         end
-    end
-
-endmodule
+            else begin
+                even = 0;
+                odd = 1;
+            end
+            end
+        endmodule
