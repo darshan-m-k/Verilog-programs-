571347e6361475afcc34e79502f5f5b52fc08c3a
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-15 13:47:39 +0530

    Update Testbench.v

--- 8-Bit Even-Odd Checker Using Always Block/Testbench.v
+++ 8-Bit Even-Odd Checker Using Always Block/Testbench.v
@@ -1,34 +1,28 @@
-module tb_even_odd_checker;
-
-  reg [7:0] n;  
-    wire even;    
-    wire odd;     
+module even_odd_checker_tb;
+  reg [7:0] n;
+  wire even,odd;     
     
-    even_odd_checker uut (
-        .n(n),
-        .even(even),
-        .odd(odd)
-    );
+  even_odd_checker dut (n,even,odd);
+  
+  initial begin
+    
+    n = 8'd2; 
+    #10;
+    $display("2 is even= %b, 2 is odd= %b", even,odd);
+    
+    n = 8'd3;
+    #10;
+    $display("3 is even= %b, 3 is odd= %b",even, odd);
+    
+    n = 8'd8;
+    #10;
+    $display("8 is even= %b, 8 is odd= %b", even,odd);
+    
+    n = 8'd15;
+    #10;
+    $display("15 is even= %b, 15 is odd= %b", even,odd);
+    
+    $finish;
+  end
+    endmodule
 
-    initial begin
-       
-        n = 8'd2; 
-        #10;
-      $display("2 is even= %b, 2 is odd= %b", even,odd);
-
-        n = 8'd3; 
-        #10;
-      $display("3 is even= %b, 3 is odd= %b",even, odd);
-
-        n = 8'd8; 
-        #10;
-      $display("8 is even= %b, 8 is odd= %b", even,odd);
-
-        n = 8'd15;
-        #10;
-      $display("15 is even= %b, 15 is odd= %b", even,odd);
-
-        $finish;
-    end
-
-endmodule
