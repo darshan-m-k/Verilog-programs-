824e8a4f4dfe6e43b3ea81d37387317352bc760b
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-15 13:39:17 +0530

    Update Testbench.v

--- Prime Number detector Using For Loop/Testbench.v
+++ Prime Number detector Using For Loop/Testbench.v
@@ -1,32 +1,31 @@
-module tb_prime_checker;
-
+module prime_detector_tb;
     reg [7:0] num;
     wire a;  
     
-    prime_checker uut (
-        .num(num),
-        .a(a)  
-    );
-
+    prime_checker dut (num,a);
+    
     initial begin
-        
         num = 8'd253; 
         #10;
-      $display("2 is prime= %b", a); 
+        $display("253 is prime= %b", a); 
 
         num = 8'd4; 
         #10;
-      $display("4 is prime= %b", a); 
+        $display("4 is prime= %b", a); 
 
         num = 8'd7; 
         #10;
-      $display("7 is prime= %b", a); 
+        $display("7 is prime= %b", a); 
 
         num = 8'd10; 
         #10;
-      $display("10 is prime= %b", a); 
+        $display("10 is prime= %b", a); 
+
+        num = 8'd3; 
+        #10;
+        $display("3 is prime= %b", a);
       
         $finish;
     end
+        endmodule
 
-endmodule
