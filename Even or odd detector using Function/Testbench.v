c7a5195057747b433b5e12d71176b793df74e25c
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-16 13:33:22 +0530

    Update Testbench.v

--- Even or odd detector using Function/Testbench.v
+++ Even or odd detector using Function/Testbench.v
@@ -1,27 +1,23 @@
-// Code your testbench here
-// or browse Examples
 `timescale 1ns / 1ps
 
 module even_checker_tb;
-
     reg [7:0] num;
     wire y;
-  eo_detector dut(
-        .num(num),
-        .y(y)
-    );
+    
+    eo_detector dut(num,y);
 
     initial begin
-       num = 0;   #10;
-        num = 1;   #10;
-        num = 2;   #10;
-        num = 3;   #10;
-        num = 10;  #10;
-        num = 255; #10;
-        num = 128; #10;
+    #10;   num = 0;   
+    #10;   num = 1;   
+    #10;    num = 2;   
+    #10;    num = 3;   
+    #10;    num = 10;  
+    #10;    num = 255; 
+    #10;    num = 128; 
         $finish;
     end
-  initial begin
-    $monitor("time=%ot,num=%b,y=%b", $time, num, y);
+  
+    initial begin
+        $monitor("time=%ot | num=%b,y=%b", $time, num, y);
   end
-endmodule
+        endmodule
