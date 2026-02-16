46279ce315471a2f1e4b6f6bf34b260738772cef
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-16 13:36:51 +0530

    Update Design.v

--- Pattern Generation_1/Design.v
+++ Pattern Generation_1/Design.v
@@ -1,11 +1,12 @@
-module star_left #(parameter N=4);
+module number_gen #(parameter N=4);
   integer i,j;
+  
   initial begin
     for(i=1;i<=N;i=i+1)begin
       for(j=1;j<=i;j=j+1)begin
         $write("\t%0d",j);
       end
-      $display();
+        $display();
     end
-  end
-endmodule
+    end
+    endmodule
