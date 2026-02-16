59c763a3c48b974b1658003d37351ff0a9ce9ddc
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-16 13:31:12 +0530

    Update Design.v

--- Even or odd detector using Function/Design.v
+++ Even or odd detector using Function/Design.v
@@ -1,11 +1,10 @@
-
 module eo_detector(num,y);
   input [7:0]num;
   output y;
   function out(input [7:0]n);
     begin
-      out = (n%2==0)?1:0;
+    out = (n%2==0)?1:0;
     end
-  endfunction
-  assign y=out(num);
+      endfunction
+    assign y=out(num);
 endmodule
