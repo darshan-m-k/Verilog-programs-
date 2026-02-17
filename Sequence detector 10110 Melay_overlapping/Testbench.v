ee167ff550516283ce5c20b0742917828984983f
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-17 12:16:47 +0530

    Update Testbench.v

--- Sequence detector 10110 Melay_overlapping/Testbench.v
+++ Sequence detector 10110 Melay_overlapping/Testbench.v
@@ -1,25 +1,14 @@
 `timescale 1ns/1ps
 
-module tb_melay_seq_10110_ov;
-
-    reg in_seq;
-    reg clk;
-    reg rst;
+module melay_seq_10110_ov_tb;
+    reg in_seq,clk,rst;
     wire det_out;
+    
+    melay_seq_10110_ov dut (in_seq,clk,rst,det_out);
 
-    // Instantiate DUT
-    melay_seq_10110_ov DUT (
-        .in_seq(in_seq),
-        .clk(clk),
-        .rst(rst),
-        .det_out(det_out)
-    );
-
-    // Clock generation (10 ns period)
     always #5 clk = ~clk;
 
     initial begin
-        // Initialize
         clk = 0;
         rst = 0;
         in_seq = 0;
@@ -27,34 +16,28 @@ module tb_melay_seq_10110_ov;
         // Apply reset
         #10 rst = 1;
 
-        // Apply input sequence:
-        // Stream: 1 0 1 1 0 1 0 1 1 0
-        // Pattern 10110 appears twice (overlapping)
         #10 in_seq = 1;
         #10 in_seq = 0;
         #10 in_seq = 1;
         #10 in_seq = 1;
-        #10 in_seq = 0;   // <-- detect here
+        #10 in_seq = 0;   
 
         #10 in_seq = 1;
         #10 in_seq = 0;
         #10 in_seq = 1;
         #10 in_seq = 1;
-        #10 in_seq = 0;   // <-- detect here
+        #10 in_seq = 0;   
 
         #20 $finish;
     end
 
-    // Monitor values
     initial begin
-        $monitor("Time=%0t | in_seq=%b | det_out=%b",
-                  $time, in_seq, det_out);
+        $monitor("Time=%0t | in_seq=%b | det_out=%b",$time, in_seq, det_out);
     end
-
-    // Dump waveform
+    
     initial begin
         $dumpfile("melay_seq_10110_ov.vcd");
-        $dumpvars(0, tb_melay_seq_10110_ov);
+        $dumpvars(0, in_seq,clk,rst,det_out);
     end
+        endmodule
 
-endmodule
