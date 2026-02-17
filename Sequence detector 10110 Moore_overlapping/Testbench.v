0eb35cdfca9f6005aafd7aa405ba1c4a394b4fb8
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-17 12:24:11 +0530

    Update Testbench.v

--- Sequence detector 10110 Moore_overlapping/Testbench.v
+++ Sequence detector 10110 Moore_overlapping/Testbench.v
@@ -1,22 +1,19 @@
-// Code your testbench here
-// or browse Examples
-//------tset bench-------
-module seq_101_mealy_ov_test;
+module seq_101_mealy_ov_tb;
 	reg in_seq,clk,rst;
 	wire det_out;
 
 moore_seq_10110_ov dut(in_seq,clk,rst,det_out);
 
 	initial begin
-	clk= 0;
-	rst= 0;
-	#12 rst= 1;
-	in_seq= 1;
+		clk= 0;
+		rst= 0;
+		#12 rst= 1;
+		in_seq= 1;
 	#10 in_seq= 0;
 	#10 in_seq= 1;
 	#10 in_seq= 1;
 	#10 in_seq= 0;
-    	#10 in_seq= 1;
+    #10 in_seq= 1;
 	#10 in_seq= 0;
 	#10 in_seq= 1;
 	#10 in_seq= 1;
@@ -25,12 +22,14 @@ moore_seq_10110_ov dut(in_seq,clk,rst,det_out);
 	end
 
 	always #5 clk= ~clk;
+	
   initial begin
-    $monitor("time=%0t,in_seq=%b,clk=%b,rst=%b,det_out=%b",$time,in_seq,clk,rst,det_out);
+	  $monitor("time=%0t | in_seq=%b | clk=%b | rst=%b | det_out=%b",$time,in_seq,clk,rst,det_out);
   end
+	
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0,in_seq,clk,rst,det_out);
   end
-  
-endmodule
+	  endmodule
+
