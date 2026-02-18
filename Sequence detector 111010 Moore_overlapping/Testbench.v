dff882c452195e621a591c706fa83073fd5f3b45
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-18 14:43:30 +0530

    Update Testbench.v

--- Sequence detector 111010 Moore_overlapping/Testbench.v
+++ Sequence detector 111010 Moore_overlapping/Testbench.v
@@ -1,8 +1,6 @@
-module tb_111010;
-
-reg clk, rst, in_seq;
-wire det_out;
-
+module moore_111010_ov_tb;
+ reg clk, rst, in_seq;
+ wire det_out;
 
  moore_111010_ov DUT(in_seq, clk, rst, det_out);
 
@@ -13,9 +11,6 @@ initial begin
  rst = 0;
  in_seq = 0;
  #10 rst = 1;
-
- // Input: 111010 -> detect
- //        111010 -> detect again
 
  #10 in_seq=1;
  #10 in_seq=1;
@@ -35,8 +30,7 @@ initial begin
 end
 
 initial begin
- $monitor("Time=%0t  In=%b  Detected=%b",
-           $time, in_seq, det_out);
+ $monitor("Time=%0t | In=%b | Detected=%b",$time, in_seq, det_out);
 end
+ endmodule
 
-endmodule
