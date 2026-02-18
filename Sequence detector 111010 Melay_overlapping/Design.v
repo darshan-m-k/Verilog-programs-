c61b6255b78b374d25f29cf33071c8f8c0ea4d77
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-18 14:28:18 +0530

    Update Design.v

--- Sequence detector 111010 Melay_overlapping/Design.v
+++ Sequence detector 111010 Melay_overlapping/Design.v
@@ -1,63 +1,71 @@
-module melay_seq_10110_ov(in_seq, clk, rst, det_out);
-
+module mealy_111010_ov(in_seq, clk, rst, det_out);
  input in_seq, clk, rst;
  output reg det_out;
-
  reg [2:0] ps, ns;
+ 
+ parameter idle   = 3'b000;
+ parameter s1     = 3'b001;
+ parameter s11    = 3'b010;
+ parameter s111   = 3'b011;
+ parameter s1110  = 3'b100;
+ parameter s11101 = 3'b101;
 
- parameter idle  = 3'b000;
- parameter s1    = 3'b001;
- parameter s10   = 3'b010;
- parameter s101  = 3'b011;
- parameter s1011 = 3'b100;
+always @(posedge clk)begin
+ if(!rst)
+  ps <= idle;
+ else
+   ps <= ns;
+end
 
- // State register
- always @(posedge clk) begin
-   if (!rst)
-     ps <= idle;
-   else
-     ps <= ns;
- end
+always @(in_seq, ps)begin
+ case(ps)
 
- // Next state & output logic
- always @(in_seq or ps) begin
-   det_out = 0;      // default
-
-   case(ps)
-
-     idle: begin
-       if (in_seq) ns = s1;
-       else ns = idle;
-     end
-
-     s1: begin
-       if (in_seq) ns = s1;
-       else ns = s10;
-     end
-
-     s10: begin
-       if (in_seq) ns = s101;
-       else ns = idle;
-     end
-
-     s101: begin
-       if (in_seq) ns = s1011;
-       else ns = s10;
-     end
-
-     s1011: begin
-       if (in_seq) begin
-         ns = s1;      // overlap allowed
+ idle: if(in_seq) begin
+        ns = s1;  det_out = 0;
        end
        else begin
-         ns = s10;
-         det_out = 1; // 10110 detected
+        ns = idle; det_out = 0;
        end
-     end
 
-     default: ns = idle;
+ s1:   if(in_seq) begin
+        ns = s11; det_out = 0;
+       end
+       else begin
+        ns = idle; det_out = 0;
+       end
 
-   endcase
- end
+ s11:  if(in_seq) begin
+        ns = s111; det_out = 0;
+       end
+       else begin
+        ns = idle; det_out = 0;
+       end
+
+ s111: if(in_seq) begin
+        ns = s111; det_out = 0;
+       end
+       else begin
+        ns = s1110; det_out = 0;
+       end
+
+ s1110: if(in_seq) begin
+         ns = s11101; det_out = 0;
+        end
+        else begin
+         ns = idle; det_out = 0;
+        end
+
+ s11101: if(in_seq) begin
+          ns = s11; det_out = 0;   
+         end
+         else begin
+          ns = idle; det_out = 1;  
+         end
+
+ default: begin
+           ns = idle; det_out = 0;
+          end
+ endcase
+  end
 
 endmodule
