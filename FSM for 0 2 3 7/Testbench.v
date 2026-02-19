9b670d41465fb28b952c80f653c930ab575befb6
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-19 17:47:21 +0530

    Update Testbench.v

--- FSM for 0 2 3 7/Testbench.v
+++ FSM for 0 2 3 7/Testbench.v
@@ -1,20 +1,24 @@
-
-module mealy_seq_101_ov_test;
+module FSM_ 0 2 3 7_tb;
   reg clk,rst;
   wire [2:0]count;
-  mealy_seq_101_ov dut(rst,clk,count);
+  
+  FSM_ 0 2 3 7 dut(rst,clk,count);
+  
   initial begin
     clk=0;
     rst=0;
     #12 rst=1;
     #100$finish;
   end
+  
   always #5 clk=~clk;
+  
   initial begin
-    $monitor("clk=%b,rst=%b,count=%b",clk,rst,count);
+    $monitor("clk=%b | rst=%b | count=%b",clk,rst,count);
   end
+  
   initial begin 
     $dumpfile("dump.vcd");
-    $dumpvars(0,mealy_seq_101_ov_test);
+    $dumpvars(0,FSM_ 0 2 3 7_tb);
   end
-endmodule
+    endmodule
