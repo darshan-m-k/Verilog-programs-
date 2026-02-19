d5c69bb7486861228097b79885a45d530fb71f6f
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-19 17:42:23 +0530

    Update Design.v

--- 4x16 decoder using 2x4 decoder/Design.v
+++ 4x16 decoder using 2x4 decoder/Design.v
@@ -1,26 +1,21 @@
 module dec2x4_en (
-  input [1:0] I,
-  input En,
-  output [3:0] D
-);
-
-assign D[0] = En & ~I[1] & ~I[0];
-assign D[1] = En & ~I[1] &  I[0];
-assign D[2] = En &  I[1] & ~I[0];
-assign D[3] = En &  I[1] &  I[0];
+  input [1:0] I,En,
+  output [3:0] D);
+  
+  assign D[0] = En & ~I[1] & ~I[0];
+  assign D[1] = En & ~I[1] &  I[0];
+  assign D[2] = En &  I[1] & ~I[0];
+  assign D[3] = En &  I[1] &  I[0];
 
 endmodule
   
 module dec4x16 (
   input [3:0] i,
-  output [15:0]d
-);
-
+  output [15:0]d);
   wire [3:0] w;  
   dec2x4_en d0 ( .I(i[3:2]),.En(1'b1),.D(w) );
   dec2x4_en d1 (.I(i[1:0]),.En(w[0]),.D(d[3:0]));
   dec2x4_en d2 (.I(i[1:0]),.En(w[1]),.D(d[7:4]) );
   dec2x4_en d3 (.I(i[1:0]),.En(w[2]),.D(d[11:8]) );
   dec2x4_en d4 (.I(i[1:0]),.En(w[3]),.D(d[15:12]));
-
 endmodule
