87e7643e2f9277531282d18c256abf08aec6bda2
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-19 17:41:28 +0530

    Update Testbench.v

--- 4-bit SIPO Asyc. with Active low reset/Testbench.v
+++ 4-bit SIPO Asyc. with Active low reset/Testbench.v
@@ -1,7 +1,7 @@
-
 module sipo_4bit_test;
   reg sin,clk,rst;
   wire [3:0]pout;
+  
   sipo_4bit dut(sin,clk,rst,pout);
   initial begin
     clk=0;
@@ -9,16 +9,19 @@ module sipo_4bit_test;
     #12 rst =1;
     sin=1;
     #10 sin =0;
-     #10 sin =1;
-     #10 sin=0;
+    #10 sin =1;
+    #10 sin=0;
     #20 $finish;
       end
+  
   always #5 clk=~clk;
+  
   initial begin
-    $monitor("time=%0t,sin=%b,clk=%b,rst=%b,pout=%b",$time,sin,clk,rst,pout);
+    $monitor("time=%0t | sin=%b | clk=%b | rst=%b | pout=%b",$time,sin,clk,rst,pout);
   end
+  
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0,sin,clk,rst,pout);
   end
-endmodule
+    endmodule
