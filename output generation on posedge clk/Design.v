12ae57d18485f013871e262616937614aaa95850
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-19 17:49:19 +0530

    Update Design.v

--- output generation on posedge clk/Design.v
+++ output generation on posedge clk/Design.v
@@ -1,12 +1,8 @@
-// Code your design here
-module d (
-    input clk,
-    input  d,
-    output reg q
-);
+module output_gen_posedge clk(
+    input clk,d,
+    output reg q);
 
 always @(posedge clk) begin
     q <= d;
 end
-
-endmodule
+    endmodule
