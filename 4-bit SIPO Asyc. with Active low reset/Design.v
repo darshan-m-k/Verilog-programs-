157a3060428763eb7c22d199baefbb3ff06225ba
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-19 17:40:05 +0530

    Update Design.v

--- 4-bit SIPO Asyc. with Active low reset/Design.v
+++ 4-bit SIPO Asyc. with Active low reset/Design.v
@@ -1,14 +1,13 @@
-// Code your design here
 module sipo_4bit(sin,clk,rst,pout);
   input sin,clk,rst;
   output reg [3:0]pout;
   reg [3:0]temp;
+  
   assign pout=temp;
-  always@(posedge clk or negedge rst)
-    begin
-      if(!rst)
-        temp<=4'b0;
-      else
-        temp <= {sin,temp[3:1]};
+  always@(posedge clk or negedge rst)begin
+    if(!rst)
+      temp<=4'b0;
+    else
+      temp <= {sin,temp[3:1]};
     end
-endmodule
+    endmodule
