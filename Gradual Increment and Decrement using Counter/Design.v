7af18fc28d35f4f0785646abbcf6a3ccf13e4bc2
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-20 12:14:52 +0530

    Update Design.v

--- Gradual Increment and Decrement using Counter/Design.v
+++ Gradual Increment and Decrement using Counter/Design.v
@@ -1,27 +1,24 @@
-// Code your design here
 module counter (clk,rst,count);
   input clk,rst;
   output reg [2:0] count;
   reg ctrl;
   
-  always @(posedge clk or negedge rst)
-    begin
-      if(!rst)
-        begin
-        count= 3'b000;
-        ctrl= 0;
-        end
-      else if(!ctrl)
-        begin
-        count= count+1;
-          if(count==7)
-            ctrl= 1;
-        end
-     else
-       begin
-        count= count-1;
-         if(count==0)
-            ctrl= 0;
-        end 
+  always @(posedge clk or negedge rst)begin
+    if(!rst)begin
+      count= 3'b000;
+      ctrl= 0;
     end
-endmodule
+      
+    else if(!ctrl)begin
+      count= count+1;
+      if(count==7)
+        ctrl= 1;
+    end
+      
+    else begin
+      count= count-1;
+      if(count==0)
+        ctrl= 0;
+    end
+    end
+    endmodule
