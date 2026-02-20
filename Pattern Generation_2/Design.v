ec36ab31e361bc3792b0a40ed0d4368b6696c6d6
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-20 12:28:13 +0530

    Add files via upload

---
+++ Pattern Generation_2/Design.v
@@ -0,0 +1,13 @@
+module tb;
+  int num = 1;
+
+  initial begin
+    for (int i = 1; i <= 4; i++) begin
+      for (int j = 1; j <= i; j++) begin
+        $write("%0d", num);
+        num++;
+      end
+      $write("\n");
+    end
+  end
+endmodule
