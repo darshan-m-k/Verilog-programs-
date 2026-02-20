18bad183636b71626a45dc56ce5c083fc0c0a0ed
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-20 12:18:18 +0530

    Update Design.v

--- Shift Register/Design.v
+++ Shift Register/Design.v
@@ -1,17 +1,15 @@
 module shift_register (
-  input wire clk,
-  input wire rst,
-  input wire data_in,
-  output reg data_out
-);
+  input wire clk,rst,data_in,
+  output reg data_out);
   reg [3:0] mem;
-
+  
   always @(posedge clk or negedge rst) begin
     if (!rst)
       mem <= 4'b0000;
     else begin
       mem <= {data_in, mem[3:1]};  
-      data_out <= mem[0];         
+      data_out <= mem[0]; 
+      end
     end
-  end
-endmodule
+    endmodule
+
