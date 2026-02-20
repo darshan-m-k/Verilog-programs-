a671257a5f134d73409191ea6676e11174f644a3
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-20 12:11:44 +0530

    Update Design.v

--- Frequency divider 2/Design.v
+++ Frequency divider 2/Design.v
@@ -1,23 +1,23 @@
-// Code your design here
 module FD#(parameter N = 5 ) (input clk, rst, output reg clk_out);
-reg int_clk;
-integer count ;
-assign clk_out = int_clk;
-always @(clk) // always @( posedge clk or negedge clk)
-begin
-if(rst)
-begin
-int_clk =0;
-count = 0;
-end
-else
-begin
-count <= count+1;
-if (count% N ==0)
-begin
-int_clk = ~int_clk;
-count = 0;
-end
-end
-end
-endmodule
+  reg int_clk;
+  integer count ;
+  
+  assign clk_out = int_clk;
+  
+  always @(clk) // always @( posedge clk or negedge clk)
+    begin
+    if(rst)begin
+      int_clk =0;
+      count = 0;
+    end
+      
+      else begin
+        count <= count+1;
+        if (count% N ==0)
+          begin
+          int_clk = ~int_clk;
+            count = 0;
+          end
+          end
+      end
+      endmodule
