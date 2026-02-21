072d25dfec77a3a18c9b3f880a362e3fb2e4b6f1
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-21 14:10:46 +0530

    Update Testbench.v

--- 2-bit N to N^2 generator/Testbench.v
+++ 2-bit N to N^2 generator/Testbench.v
@@ -1,22 +1,24 @@
 module square_generator_tb;
  reg A, B;
  wire [3:0] D;
- square_generator
-dut(.A(A), .B(B), .D(D));
+ square_generator dut(A,B,D);
+ 
  initial begin
- A = 0; B = 0;
- #5 A = 0; B = 1;
- #5 A = 1; B = 0;
- #5 A = 1; B = 1;
- #5 A = 1; B = 0;
+  A = 0; B = 0;
+  #5 A = 0; B = 1;
+  #5 A = 1; B = 0;
+  #5 A = 1; B = 1;
+  #5 A = 1; B = 0;
  #5;
  end
+ 
  initial begin
- $monitor("Simtime=%0t, A=%b,
-B=%b -> D=%b", $time, A, B, D);
+  $monitor("Simtime=%0t | A=%b | B=%b -> D=%b", $time, A, B, D);
  end
+ 
  initial begin
- $dumpfile("square_generator.vcd");
- $dumpvars(0, A, B, D);
+  $dumpfile("square_generator.vcd");
+  $dumpvars(0, A, B, D);
  end
-endmodule
+  endmodule
+
