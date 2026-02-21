ca9511788c1b0e26f75b9ebfe1bf8c224d0be40b
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-21 14:25:22 +0530

    Update Testbench.v

--- Reduction operators/Testbench.v
+++ Reduction operators/Testbench.v
@@ -1,14 +1,10 @@
 module reduction_operators_tb;
  reg [3:0] A;
- wire and_red, or_red, xor_red,
-nand_red, nor_red, xnor_red;
- reduction_operators uut
-(.A(A), .and_red(and_red), .or_red(or_red
-), .xor_red(xor_red), .nand_red(nand_re
-d), .nor_red(nor_red), .xnor_red(xnor_re
-d));
+ wire and_red, or_red, xor_red,nand_red, nor_red, xnor_red;
+ reduction_operators dut(A,and_red, or_red, xor_red,nand_red, nor_red, xnor_red);
+ 
  initial begin
-A = 4'b0000; #5;
+ A = 4'b0000; #5;
  A = 4'b0001; #5;
  A = 4'b0011; #5;
  A = 4'b0101; #5;
@@ -19,15 +15,13 @@ A = 4'b0000; #5;
  A = 4'b1xz0; #5;
  A = 4'b0xz0; #5;
  end
+ 
  initial begin
- $monitor("Time=%0t A=%b | &=%b
-|=%b ^=%b ~&=%b ~|=%b ~^=%b",
- $time, A, and_red, or_red,
-xor_red, nand_red, nor_red, xnor_red);
+  $monitor("Time=%0t A=%b | &=%b|=%b ^=%b ~&=%b ~|=%b ~^=%b",$time, A, and_red, or_red,xor_red, nand_red, nor_red, xnor_red);
  end
+ 
  initial begin
- $dumpfile("reduction_operators.vcd");
- $dumpvars(0, A, and_red, or_red,
-xor_red, nand_red, nor_red, xnor_red);
+  $dumpfile("reduction_operators.vcd");
+  $dumpvars(0, A, and_red, or_red,xor_red, nand_red, nor_red, xnor_red);
  end
-endmodule
+  endmodule
