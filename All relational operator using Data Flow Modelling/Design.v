d0512a3cd6ee925393cb5c11c53521cfe795a197
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-21 14:12:20 +0530

    Update Design.v

--- All relational operator using Data Flow Modelling/Design.v
+++ All relational operator using Data Flow Modelling/Design.v
@@ -1,13 +1,11 @@
 module relational_operators (
  input [3:0] A, B,
- output eq, neq, gt, lt, gte, lte
-);
+ output eq, neq, gt, lt, gte, lte);
+ 
  assign eq = (A == B); // Equal
  assign neq = (A != B); // Not equal
  assign gt = (A > B); // Greater than
  assign lt = (A < B); // Less than
- assign gte = (A >= B); // Greater than
-or equal
- assign lte = (A <= B); // Less than or
-equal
+ assign gte = (A >= B); // Greater than or equal
+ assign lte = (A <= B); // Less than or equal
 endmodule
