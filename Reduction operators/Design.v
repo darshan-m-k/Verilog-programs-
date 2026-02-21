7b53c6b03430ad5b767794b156713acc3c51f2eb
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-21 14:23:54 +0530

    Update Design.v

--- Reduction operators/Design.v
+++ Reduction operators/Design.v
@@ -1,8 +1,7 @@
 module reduction_operators (
  input [3:0] A,
- output and_red, or_red, xor_red,
-nand_red, nor_red, xnor_red
-);
+ output and_red, or_red, xor_red,nand_red, nor_red, xnor_red);
+ 
  assign and_red = &A;
  assign or_red = |A;
  assign xor_red = ^A;
@@ -10,3 +9,4 @@ nand_red, nor_red, xnor_red
  assign nor_red = ~|A;
  assign xnor_red = ~^A;
 endmodule
+
