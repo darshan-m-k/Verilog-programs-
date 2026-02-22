aed3e5d7bd81c57cb2be5dc3a2484dd7bae7bb4c
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-22 17:31:37 +0530

    Update Design.v

--- All Logical operator/Design.v
+++ All Logical operator/Design.v
@@ -1,11 +1,8 @@
 module logical_operators_4bit (
  input [3:0] A, B,
- output and_op, or_op, not_op
-);
- assign and_op = A && B; // True if
-both A and B are non-zero
- assign or_op = A || B; // True if either
-A or B is non-zero
- assign not_op = !A; // True if A is
-zero
+ output and_op, or_op, not_op);
+ 
+ assign and_op = A && B; // True if both A and B are non-zero
+ assign or_op = A || B; // True if either A or B is non-zero
+ assign not_op = !A; // True if A is zero
 endmodule
