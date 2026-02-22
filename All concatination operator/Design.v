6112b8f50599136cb10e9425c83e4df59e81d25e
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-22 17:34:14 +0530

    Update Design.v

--- All concatination operator/Design.v
+++ All concatination operator/Design.v
@@ -1,12 +1,7 @@
 module concatenation_operators (
- input [3:0] A,
- input [3:0] B,
- output [7:0] concat_result,
- output [7:0] replicate_result
-);
- assign concat_result = {A, B}; //
-Concatenation: A followed by B
- assign replicate_result =
-{2{A}}; // Replication: A repeated 2
-times
+ input [3:0] A,B,
+ output [7:0] concat_result,replicate_result);
+ 
+ assign concat_result = {A, B}; //Concatenation: A followed by B
+ assign replicate_result ={2{A}}; // Replication: A repeated 2 times
 endmodule
