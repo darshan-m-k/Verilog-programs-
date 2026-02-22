33a80294700590649e869fbd8f93bb72f10eb162
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-22 17:30:08 +0530

    Add files via upload

---
+++ D-latch using 2x1mux using Structural Modelling/Testbench.v
@@ -0,0 +1,24 @@
+dule dlatch_tb;
+ reg d,clk,q1;
+ wire q;
+ dlatch_mux d1(d,q,clk);
+ initial begin
+ {clk,d,q1} = 3'b000; #5
+ {clk,d,q1} = 3'b001; #5
+ {clk,d,q1} = 3'b010; #5
+ {clk,d,q1} = 3'b011; #5
+ {clk,d,q1} = 3'b100; #5
+ {clk,d,q1} = 3'b101; #5
+ {clk,d,q1} = 3'b000; #5
+ {clk,d,q1} = 3'b001; #5
+ {clk,d,q1} = 3'b110; #5
+ {clk,d,q1} = 3'b111; #5
+ {clk,d,q1} = 3'b000; #5
+ {clk,d,q1} = 3'b001; #5;
+ end
+ initial begin
+ $monitor("sim time: %t, d =%b,
+clk=%b, q1 = %b, q = %b",
+$time,d,clk,q1, q);
+ end
+endmodule
