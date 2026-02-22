b380bd474ebb4b8ce180e40651d1848852ce1c38
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-22 17:36:56 +0530

    Update Design.v

--- D-latch using 2x1mux using Structural Modelling/Design.v
+++ D-latch using 2x1mux using Structural Modelling/Design.v
@@ -1,15 +1,21 @@
 module mux2_1(i,s,y);
- input [1:0] i; input s; output y;
+ input [1:0] i; input s;
+ output y;
+ 
  assign y = s?i[1]:i[0];
 endmodule
+ 
 module dlatch_mux(d,q,clk);
- input d, clk; output q;
+ input d, clk; 
+ output q;
  wire q1;
  mux2_1 m1({d,q1},clk,q1);
  assign q = q1;
 endmodule
+
 module dff(d,q,clk);
- input d, clk; output q;
+ input d, clk; 
+ output q;
  wire q1,a1;
  mux2_1 m1({d,a1},clk,a1);
  mux2_1 m2({a1,q1},clk,q1);
