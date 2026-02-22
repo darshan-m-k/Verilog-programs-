4d4b2cf167424d858f9a0cbcc77291a7db1489dc
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-22 17:37:46 +0530

    Update Testbench.v

--- D-latch using 2x1mux using Structural Modelling/Testbench.v
+++ D-latch using 2x1mux using Structural Modelling/Testbench.v
@@ -1,7 +1,8 @@
-dule dlatch_tb;
+module dlatch_tb;
  reg d,clk,q1;
  wire q;
- dlatch_mux d1(d,q,clk);
+
+ dlatch_mux dut(d,q,clk);
  initial begin
  {clk,d,q1} = 3'b000; #5
  {clk,d,q1} = 3'b001; #5
@@ -16,9 +17,9 @@ dule dlatch_tb;
  {clk,d,q1} = 3'b000; #5
  {clk,d,q1} = 3'b001; #5;
  end
+
  initial begin
- $monitor("sim time: %t, d =%b,
-clk=%b, q1 = %b, q = %b",
-$time,d,clk,q1, q);
+ $monitor("sim time: %t, d =%b,clk=%b, q1 = %b, q = %b",$time,d,clk,q1, q);
  end
-endmodule
+  endmodule
+
