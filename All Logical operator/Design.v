4c022b459701665eaae2f00f97119709414df0ea
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-22 17:29:15 +0530

    Add files via upload

---
+++ All Logical operator/Design.v
@@ -0,0 +1,11 @@
+module logical_operators_4bit (
+ input [3:0] A, B,
+ output and_op, or_op, not_op
+);
+ assign and_op = A && B; // True if
+both A and B are non-zero
+ assign or_op = A || B; // True if either
+A or B is non-zero
+ assign not_op = !A; // True if A is
+zero
+endmodule
---
+++ All Logical operator/Testbench.v
@@ -0,0 +1,31 @@
+module logical_operators_4bit_tb;
+ reg [3:0] A, B;
+ wire and_op, or_op, not_op;
+ logical_operators_4bit uut
+(.A(A), .B(B), .and_op(and_op), .or_op(or_
+op), .not_op(not_op));
+ initial begin
+ // Directed test cases covering all key
+scenarios including x and z
+ A = 4'b0000; B = 4'b0000; #5;
+ A = 4'b0000; B = 4'b0001; #5;
+ A = 4'b0010; B = 4'b0000; #5;
+ A = 4'b1111; B = 4'b1111; #5;
+ A = 4'bxxxx; B = 4'b0001; #5;
+ A = 4'b0000; B = 4'bxxxx; #5;
+ A = 4'bx11x; B = 4'bxxxx; #5;
+ A = 4'bzzzz; B = 4'b0001; #5;
+ A = 4'b0001; B = 4'bzzzz; #5;
+ A = 4'bzzzz; B = 4'bzzzz; #5;
+ A = 4'bzzzz; B = 4'bxxxx; #5;
+ A = 4'bx0xx; B = 4'bz01z; #5;
+ end
+ initial begin
+ $monitor("Time=%0t A=%b B=%b |
+A&&B=%b A||B=%b !A=%b", $time, A, B,
+and_op, or_op, not_op);
+ end
+ initial begin
+$dumpfile("logical_operators_4bit.vcd");
+ $dumpvars(0, A, B, and_op, or_op,
+not_op);
