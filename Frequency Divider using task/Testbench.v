12ea8081e4ce13169f7e950077a9d03bd91dc1cc
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-23 12:34:07 +0530

    Update Testbench.v

--- Frequency Divider using task/Testbench.v
+++ Frequency Divider using task/Testbench.v
@@ -1,12 +1,17 @@
-module test_fd_task;
-wire clk_out;
-reg clk, rst;
-FD Dut(clk, rst, clk_out);
-initial begin
-clk = 0;
-rst= 1;
-#13 rst = 0;
-#200 $finish;
-end
-always #5 clk =~clk;
-endmodule
+module FD_task_tb;
+  wire clk_out;
+  reg clk, rst;
+  FD dut(clk, rst, clk_out);
+  initial begin
+    clk = 0;
+    rst= 1;
+    #13 rst = 0;
+    #200 $finish;
+  end
+    always #5 clk =~clk;
+  
+  initial begin
+ $dumpfile("dump.vcd");
+ $dumpvars(1,clk, rst, clk_out);
+ end
+    endmodule
