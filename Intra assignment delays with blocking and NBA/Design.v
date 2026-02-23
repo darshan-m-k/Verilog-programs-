be56c0b6874024fd0635f8503c2916ef079bf2c2
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-23 12:48:29 +0530

    Update Design.v

--- Intra assignment delays with blocking and NBA/Design.v
+++ Intra assignment delays with blocking and NBA/Design.v
@@ -1,13 +1,16 @@
-module delay(
+//for Non-blocking
+module Intra_delay(
  input signed [31:0] a,
  output integer b);
  always @ (a)
   b <= #3 a;
 endmodule
- 
-module delay(
+
+//for Blocking
+module Intra_delay(
  input signed [31:0] a,
  output integer b);
  always @ (a)
   b = #3 a;
 endmodule
+
