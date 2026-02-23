5004a2137bc99d3ec3aaec572e60833eb552aab2
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-23 12:42:27 +0530

    Update Testbench.v

--- Higher order decoder using lower order decoder with Enable switch/Testbench.v
+++ Higher order decoder using lower order decoder with Enable switch/Testbench.v
@@ -1,25 +1,28 @@
-module test_dec4_16;
-wire [0:15]D;
-reg [3:0] I;
-reg [31:0] i;
-dec4_16 Dut(I,D);
-initial begin
-for( i = 0 ; i<16; i=i+1)
-begin
-I = i;
-#5 I = i;
-end
-end
-initial begin
+module dec4_16_tb;
+  wire [0:15]D;
+  reg [3:0] I;
+  reg [31:0] i;
+  
+  dec4_16 Dut(I,D);
+  initial begin
+    for( i = 0 ; i<16; i=i+1)
+      begin
+      I = i;
+   #5 I = i;
+      end
+      end
+    
+  initial begin
 $monitor("I = %b, D= %b", I, D);
-end
-endmodule
-module test_dec3_8;
-wire [0:7]D;
-reg [2:0] I;
-dec3_8 Dut(I,D);
-initial begin
-I = 3'b000;
+  end
+    endmodule
+  
+module dec3_8_tb;
+  wire [0:7]D;
+  reg [2:0] I;
+  dec3_8 dut(I,D);
+  initial begin
+    I = 3'b000;
 #5 I = 3'b001;
 #5 I = 3'b010;
 #5 I = 3'b011;
@@ -28,7 +31,7 @@ I = 3'b000;
 #5 I = 3'b110;
 #5 I = 3'b111;
 end
-initial begin
+    initial begin
 $monitor("I = %b, D= %b", I, D);
 end
-endmodule
+      endmodule
