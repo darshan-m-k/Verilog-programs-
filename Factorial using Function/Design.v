722b24c77160dd10cc8efa1e569c5b48bcc1819e
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-23 12:29:14 +0530

    Update Design.v

--- Factorial using Function/Design.v
+++ Factorial using Function/Design.v
@@ -1,12 +1,14 @@
-module factorial (input [31:0] N, output [63:0]
-facto);
-assign facto = fact(N);
-function automatic[63:0] fact (input [31:0] N);
-begin
-if(N>1)
-fact = fact(N-1) *N;
-else
-fact = 1;
-end
-endfunction
-endmodule
+module factorial (
+  input [31:0] N,
+  output [63:0]facto);
+  
+  assign facto = fact(N);
+  function automatic[63:0] fact (input [31:0] N);
+    begin
+    if(N>1)
+      fact = fact(N-1) *N;
+      else
+      fact = 1;
+    end
+      endfunction
+    endmodule
