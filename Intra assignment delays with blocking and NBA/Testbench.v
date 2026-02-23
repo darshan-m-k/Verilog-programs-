668c60c34503f136c7e6f9d8b4c1171146afeffe
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-23 12:49:30 +0530

    Update Testbench.v

--- Intra assignment delays with blocking and NBA/Testbench.v
+++ Intra assignment delays with blocking and NBA/Testbench.v
@@ -1,10 +1,12 @@
-module delay_tb;
-reg signed [31:0] a;
-wire integer b;
-delay d1(a, b);
-initial begin
+module Intra_delay_tb;
+ reg signed [31:0] a;
+ wire integer b;
+ 
+Intra_delay dut(a, b);
+ 
+ initial begin
  a = 10;
- $monitor("Simtime=%t,
-a=%d, b=%d", $time, a, b);
- #4 a = 15;
-end endmodule
+  $monitor("Simtime=%t | a=%d | b=%d", $time, a, b);
+  #4 a = 15;
+ end
+  endmodule
