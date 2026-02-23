7c11752356704309cf7991c8beb95f747c5d0cda
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-23 12:36:29 +0530

    Update Testbench.v

--- Gate level Delays with all parameters/Testbench.v
+++ Gate level Delays with all parameters/Testbench.v
@@ -1,17 +1,15 @@
-// Code your testbench here
-// or browse Examples
-module tb;
+module delays_tb;
  reg a, b;
  wire o1, o2;
- my_design dut (a, b, o1, o2);
+ delays dut (a, b, o1, o2);
+ 
  initial begin
- a = 0; b = 0;
- $monitor ("T=%0t a=%0b
-b=%0b out1=%0b out2=%0b",
-$time, a, b, o1, o2);
- #10 a <= 1;
- #10 b <= 1;
- #10 a <= 0;
- #10 b <= 0;
+  a = 0; b = 0;
+  $monitor ("T=%0t | a=%0b | b=%0b | out1=%0b | out2=%0b",$time, a, b, o1, o2);
+  #10 a <= 1;
+  #10 b <= 1;
+  #10 a <= 0;
+  #10 b <= 0;
  end
-endmodule
+  endmodule
+
