29a0583dc767dc66eafdbbd0bed76f824718ae6b
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-23 12:31:53 +0530

    Update Design.v

--- Frequency Divider using task/Design.v
+++ Frequency Divider using task/Design.v
@@ -1,26 +1,29 @@
-module FD_task#(parameter N = 5 ) (input clk,
-rst, output reg clk_out);
-reg int_clk;
-integer count ;
-assign clk_out = int_clk;
-task cg(); begin
-if(rst) begin
-int_clk =0;
-count = 0;
-end
-else
-begin
-count <= count+1;
-if (count% N ==0)
-begin
-int_clk = ~int_clk;
-count = 0;
-end
-end
-end
-endtask
-always @(clk) begin // always @( posedge clk or
-negedge clk)
-cg();
-end
-endmodule
+module FD_task#(parameter N = 5 )(
+  input clk,rst, 
+  output reg clk_out);
+  reg int_clk;
+  integer count ;
+  
+  assign clk_out = int_clk;
+  task cg(); 
+    begin
+    if(rst) begin
+      int_clk =0;
+      count = 0;
+    end
+      else
+      begin
+        count <= count+1;
+        if (count% N ==0)
+          begin
+          int_clk = ~int_clk;
+            count = 0;
+          end
+          end
+      end
+      endtask
+  always @(clk) begin // always @( posedge clk or negedge clk)
+    cg();
+  end
+    endmodule
+
