d25bcf0aa1a1360bbf550e88d10830122801b412
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-23 12:44:06 +0530

    Update Design.v

--- Inter assignment delays with blocking and NBA/Design.v
+++ Inter assignment delays with blocking and NBA/Design.v
@@ -1,6 +1,11 @@
-module delay(input signed [31:0] a, output integer b);
-always @ (a) #3 b = a;
+module delay(
+  input signed [31:0] a, 
+  output integer b);
+  always @ (a) #3 b = a;
 endmodule
+
 //(OR) same output
+
 module delay(input signed [31:0] a, output integer b);
-always @ (a
+  always @ (a) #3 b <= a;
+endmodule
