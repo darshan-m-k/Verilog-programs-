9ea70aae427e6c91d7f89b3f6066f1d144f1d13c
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-23 12:30:06 +0530

    Update Testbench.v

--- Factorial using Function/Testbench.v
+++ Factorial using Function/Testbench.v
@@ -1,13 +1,14 @@
-module fact_tb;
-reg [31:0] N;
-wire [63:0] facto;
-factorial dut(N,facto);
-initial begin
-N= 3;
-#2 N=2;
-#3 N=4;
-end
-initial begin
-$monitor(N,facto);
-end
-endmodule
+module factorial_tb;
+  reg [31:0] N;
+  wire [63:0] facto;
+  
+  factorial dut(N,facto);
+  initial begin
+    N= 3;
+    #2 N=2;
+    #3 N=4;
+  end
+    initial begin
+      $monitor(N,facto);
+    end
+      endmodule
