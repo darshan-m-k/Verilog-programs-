f38cdea9795cdcdc297088ccd67faecc86a032c9
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-24 19:09:03 +0530

    Update Design.v

--- Generating a colck of 3ns and 5ns/Design.v
+++ Generating a colck of 3ns and 5ns/Design.v
@@ -1,18 +1,17 @@
 `timescale 1ns/1ps
-module test;
-reg clk1;
-  reg clk2;
-initial begin
+module tb;
+  reg clk1,clk2;
   
-clk1 = 0;
-      clk2 = 0;
-      #50 $finish;
-end
-always #1.5 clk1 = ~clk1;
+  initial begin
+    clk1 = 0;
+    clk2 = 0;
+    #50 $finish;
+  end
+  always #1.5 clk1 = ~clk1;
   always #2.5 clk2 = ~clk2;
   
   initial begin 
-      $dumpfile("Dump.vcd");
-      $dumpvars(0, clk1,clk2); 
+    $dumpfile("Dump.vcd");
+    $dumpvars(0, clk1,clk2); 
   end
-endmodule
+    endmodule
