367e12d031bd65b4ef48ed85c7e58098ad4bc793
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-24 19:10:06 +0530

    Update Design.v

--- Procedural continuous Assignment/Design.v
+++ Procedural continuous Assignment/Design.v
@@ -1,8 +1,10 @@
-module dff (input din, rst, clk, output reg q);
-always @ (posedge clk)
-q<=din;
+module dff (
+  input din, rst, clk, 
+  output reg q);
+  always @ (posedge clk)
+    q<=din;
   always @ (rst) begin
     if (rst)  assign q=1'b0;
-else deassign q;  //Next assignment at posedge clk
+    else deassign q;  //Next assignment at posedge clk
   end
-endmodule
+    endmodule
