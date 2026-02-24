84b96a0d0681bedd5cbaf80b92aebdabd03a0a62
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-24 19:00:31 +0530

    Update Design.v

--- Comparing Intra and Inter, BA and NBA/Design.v
+++ Comparing Intra and Inter, BA and NBA/Design.v
@@ -1,15 +1,16 @@
 module delay_tb; //(blocking)
   reg signed [31:0] a, b, c, d;
-initial begin
+  initial begin
     #2 a = 0;
     #3 b = 1;
     #4 c = 2;
     #5 d = 3;
 end
   initial begin
-    $monitor("Simtime=%0t, a=%0d, b=%0d,c=%0d, 
-d=%0d", $time, a, b,c,d);
-end endmodule
+    $monitor("Simtime=%0t | a=%0d | b=%0d | c=%0d | d=%0d", $time, a, b,c,d);
+  end 
+endmodule
+
 module delay_tb; //(Non- blocking)
   reg signed [31:0] a, b, c, d;
 initial begin
@@ -19,6 +20,6 @@ initial begin
     #5 d <= 3;
 end
   initial begin
-    $monitor("Simtime=%0t, a=%0d, b=%0d,c=%0d, 
-d=%0d", $time, a, b,c,d);
-end endmodule
+    $monitor("Simtime=%0t | a=%0d | b=%0d | c=%0d | d=%0d", $time, a, b,c,d);
+  end
+    endmodule
