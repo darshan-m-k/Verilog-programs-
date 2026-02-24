4630a51835327c7b2c67fe4fa33c41ac93ec3b53
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-24 19:03:02 +0530

    Update Testbench.v

--- Example_1 for fork join/Testbench.v
+++ Example_1 for fork join/Testbench.v
@@ -1,14 +1,14 @@
-module test;
-initial fork
-   #5 $display($time, Thread A);
-begin
-#4 $display($time, Thread B);
-#1 $display($time, Thread C);
-fork
-#2 $display($time, Thread D);
-#5 $display($time, Thread E);
-join
-$display($time, Thread F);
-end
-join
-   endmodule
+module tb;
+   initial fork
+      #5 $display($time, Thread A);
+      begin
+         #4 $display($time, Thread B);
+         #1 $display($time, Thread C);
+         fork
+            #2 $display($time, Thread D);
+            #5 $display($time, Thread E);
+         join
+            $display($time, Thread F);
+         end
+         join
+      endmodule
