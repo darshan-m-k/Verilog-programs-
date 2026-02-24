649ffd8da72e9e14327f5362baf2a94d44f3711a
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-24 18:56:36 +0530

    Add files via upload

---
+++ Comparing Intra and Inter, BA and NBA/Design.v
@@ -0,0 +1,24 @@
+module delay_tb; //(blocking)
+  reg signed [31:0] a, b, c, d;
+initial begin
+    #2 a = 0;
+    #3 b = 1;
+    #4 c = 2;
+    #5 d = 3;
+end
+  initial begin
+    $monitor("Simtime=%0t, a=%0d, b=%0d,c=%0d, 
+d=%0d", $time, a, b,c,d);
+end endmodule
+module delay_tb; //(Non- blocking)
+  reg signed [31:0] a, b, c, d;
+initial begin
+    #2 a <= 0;
+    #3 b <= 1;
+    #4 c <= 2;
+    #5 d <= 3;
+end
+  initial begin
+    $monitor("Simtime=%0t, a=%0d, b=%0d,c=%0d, 
+d=%0d", $time, a, b,c,d);
+end endmodule
---
+++ Comparing Intra and Inter, BA and NBA/Testbench.v
@@ -0,0 +1,24 @@
+module delay_tb; //(Non- blocking)
+  reg signed [31:0] a, b, c, d;
+initial begin
+     a <= #2 0;
+     b <= #3 1;
+     c <= #4 2;
+     d <= #5 3;
+end
+  initial begin
+    $monitor("Simtime=%0t, a=%0d, 
+b=%0d,c=%0d, d=%0d", $time, a, b,c,d);
+end endmodule
+module delay_tb;
+  reg signed [31:0] a, b, c, d;
+initial begin
+     a = #2 0;
+   b = #3 1;
+     c = #4 2;
+     d = #5 3;
+end
+  initial begin
+    $monitor("Simtime=%0t, a=%0d, 
+b=%0d,c=%0d, d=%0d", $time, a, b,c,d);
+end endmodule
