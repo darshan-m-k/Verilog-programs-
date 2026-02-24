2b796c065e83b918c343144724d238dc8e51d1df
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-24 19:04:22 +0530

    Update Testbench.v

--- Example_2 for fork join/Testbench.v
+++ Example_2 for fork join/Testbench.v
@@ -1,19 +1,19 @@
- module test;
-initial fork
+module tb;
+ initial fork
    #5 $display($time, "Thread A");
-begin
-#4 $display($time, "Thread B");
-#1 $display($time, "Thread C");
-fork
-#2 $display($time, "Thread D");
-fork
-#5 $display($time, "Thread E");
-#2 $display($time, "Thread F");
-join
-#1 $display($time, "Thread G");
-join
-$display($time, "Thread H");
-end
-       $display($time, "Thread I"); 
-join
-   endmodule
+  begin
+   #4 $display($time, "Thread B");
+   #1 $display($time, "Thread C");
+   fork
+    #2 $display($time, "Thread D");
+    fork
+     #5 $display($time, "Thread E");
+     #2 $display($time, "Thread F");
+    join
+    #1 $display($time, "Thread G");
+   join
+    $display($time, "Thread H");
+  end
+   $display($time, "Thread I"); 
+ join
+  endmodule
