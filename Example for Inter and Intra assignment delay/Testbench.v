610c735d8fb48af65dd00b9c08c8f52d4dc88762
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-24 19:31:06 +0530

    Add files via upload

---
+++ Example for Inter and Intra assignment delay/Testbench.v
@@ -0,0 +1,14 @@
+module test;
+integer a,b,c,d;
+initial begin
+#2 a<= #1 3; 
+#1 b= #4 5; 
+#5 c= #2 b+a;
+#2 d<= #3 10;
+   d= #5 a+b;
+end
+initial begin
+$monitor("Sim time= %0t, A= %0d, b=%0d, 
+c=%0d, d= %0d", $time,a,b,c,d);
+end
+    endmodule
