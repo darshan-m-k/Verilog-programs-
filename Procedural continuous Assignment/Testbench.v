6a1c7fd13dd244fec03a194b009e312fe13541fc
Darshan M K <150264681+darshan-m-k@users.noreply.github.com>
2026-02-24 19:11:52 +0530

    Update Testbench.v

--- Procedural continuous Assignment/Testbench.v
+++ Procedural continuous Assignment/Testbench.v
@@ -1,19 +1,26 @@
 module dff_tb;
-  reg din; reg rst; reg clk;  wire q;
-  dff uut ( .din(din), .rst(rst), .clk(clk), 
-.q(q) );
-initial begin clk = 0;
+  reg din; 
+  reg rst,clk;  
+  wire q;
+  dff dut (din,rst,clk,q);
+  
+  initial begin clk = 0;
     forever #5 clk = ~clk; 
   end
+  
   initial begin
-    din = 0; rst = 0;
-    #3 rst = 1; #7 rst = 0;
-    #10 din = 1;  #10 din = 0;
-    #5 rst = 1;  #10 din = 1;
-    #10 din = 0; #20 $finish;
+    din = 0; 
+    rst = 0;
+    #3 rst = 1;
+    #7 rst = 0;
+    #10 din = 1; 
+    #10 din = 0;
+    #5 rst = 1;  
+    #10 din = 1;
+    #10 din = 0; 
+    #20 $finish;
   end
   initial begin
-    $monitor("Time=%0t rst=%b 
-din=%b q=%b", $time, rst, din, q);
+    $monitor("Time=%0t | rst=%b | din=%b | q=%b", $time, rst, din, q);
   end
-endmodule
+    endmodule
